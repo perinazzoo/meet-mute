e202f56ae8c188d94b06c69d69f53303ca7f4684
perinazzoo <gabrielperinazo@gmail.com>
2020-08-20 11:40:24 -0300

    :tada: Initial commit

---
+++ popup.js
@@ -0,0 +1,54 @@
+let mute = document.querySelector("#mute");
+let volume;
+
+const code = `(function getUrls(){
+  const audioList = [];
+
+  document.querySelectorAll('audio').forEach((el) => audioList.push(el.volume));
+  const href = window.location.href;
+  return { audioList, href };
+})()`;
+
+window.onload = function () {
+  checkIfHasVolume();
+}
+
+mute.onclick = () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.executeScript(
+      tabs[0].id,
+      { code: `document.querySelectorAll("audio").forEach((aud) => aud.volume = ${volume ? 0 : 1})` });
+  });
+
+  checkIfHasVolume();
+}
+
+function changeColor() {
+  if (volume) {
+    mute.style.backgroundColor = "#DF2935";
+    return;
+  }
+  mute.style.backgroundColor = "#26C485";
+}
+
+function callBackAudioList(result) {
+  const { audioList } = result[0];
+
+  const anyVolumeOn = audioList.some((aud) => aud > 0);
+
+  volume = anyVolumeOn;
+
+  mute.innerHTML = volume ? 'Mute' : 'Unmute';
+
+  changeColor();
+}
+
+function checkIfHasVolume() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.executeScript(
+      tabs[0].id,
+      { code },
+      callBackAudioList
+    )
+  });
+}
